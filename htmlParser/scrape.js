e325e856353c888604c4d7bbdb876adefc51fb58
tanja <tanjamilososka@ua.pt>
2021-12-22 15:58:55 +0000

    updated scrapper

--- htmlParser/scrape.js
+++ htmlParser/scrape.js
@@ -1,12 +1,10 @@
 const fs = require('fs')
 const cheerio=require('cheerio');
-const request = require('request');
-const { get } = require('request');
 
-
-const testFolder = './sources';
+const sourceFolder = './sources';
 let information=[];
-fs.readdir(testFolder, (err, files) => {
+
+fs.readdir(sourceFolder, (err, files) => {
         files.forEach(file => {
             let inf={};
             const html=fs.readFileSync(`sources/${file}`).toString();
@@ -20,21 +18,23 @@ fs.readdir(testFolder, (err, files) => {
                 var website=$(el).find('.col-sm-4').find("a").text();
 
                 const inf_block=$(el).find('#details-'+i.toString());
-                var adress="";
+
+                var address="";
                 var zip="";
                 var mail="";
                 var mail2="";
                 var phone="";
                 var website2="";
+
                 inf_block.find("div").each(function(j, el1) {
                     const element=$(el1);
                     if(element.text().includes("Adresas"))
-                        adress=element.find("strong").text();
+                        address=element.find("strong").text();
                     else if(element.text().includes("Zip"))
                         zip=element.find("strong").text();
                     else if(element.text().includes("paštas"))
                     {
-                        if(mail=="")
+                        if(mail==="")
                             {
                                 mail=element.find("a").text();
                                 if(mail.includes(","))
@@ -67,14 +67,12 @@ fs.readdir(testFolder, (err, files) => {
                         state : state_name,
                         location : location,
                         website : website,
-                        adress : adress,
+                        adress : address,
                         zip : zip,
                         mail: mail,
                         phone : phone,
                     }
-
                 information.push(inf);
-
             });
     });
     var obj=Object.assign({},information);
