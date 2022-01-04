30db4358ffd7f45f42717daa2c25802ac0e300ef
tanja <tanjamilososka@ua.pt>
2022-01-04 16:49:59 +0000

    expandable companies information table

--- wt-frontend/src/components/LookupTable/LookupTable.jsx
+++ wt-frontend/src/components/LookupTable/LookupTable.jsx
@@ -1,53 +1,104 @@
 import MaterialTable from "@material-table/core";
+import { Grid, Typography } from "@mui/material";
 
-const LookupTable = ({companies}) => {
+const LookupTable = ({ companies }) => {
   const columns = [
     {
       field: "name",
       title: "Name",
     },
     {
-      field: "address",
-      title: "Address",
+      field: "mail",
+      title: "Email",
     },
+    // {
+    //   field: "address",
+    //   title: "Address",
+    // },
     {
       field: "location",
       title: "Location",
     },
-    {
-      field: "mail",
-      title: "Email",
-    },
-    {
-      field: "phone",
-      title: "Phone",
-    },
-    {
-      field: "pricing",
-      title: "Pricing",
-    },
-    {
-      field: "rating",
-      title: "Rating",
-    },
+
+    // {
+    //   field: "phone",
+    //   title: "Phone",
+    // },
+    // {
+    //   field: "pricing",
+    //   title: "Pricing",
+    // },
+    // {
+    //   field: "rating",
+    //   title: "Rating",
+    // },
     {
       field: "state",
       title: "State",
     },
-    {
-      field: "website",
-      title: "Website",
-    },
-    {
-      field: "worktype",
-      title: "Work Type",
-    },
-    {
-      field: "zip",
-      title: "Zip",
-    },
+    // {
+    //   field: "website",
+    //   title: "Website",
+    // },
+    // {
+    //   field: "worktype",
+    //   title: "Work Type",
+    // },
+    // {
+    //   field: "zip",
+    //   title: "Zip",
+    // },
   ];
-  return <MaterialTable columns={columns} data={companies} title="Companies" />;
+
+  const renderWebsites = (websiteString) => {
+    const websites = websiteString.split("|")
+    if (websites.length === 1)
+    {
+      return <Typography><b>Website:</b> {websiteString}</Typography>;
+    }
+    return <div>
+      <Typography><b>Websites:</b> </Typography>
+      {websites.map(el => {
+        return <Typography>{el}</Typography>
+      })}
+    </div>
+  }
+  const calculatePricing = (pricing) => {
+    if (pricing === 1) return <Typography><b>Pricing:</b> $</Typography>
+    if (pricing === 2) return <Typography><b>Pricing:</b> $$</Typography>
+    if (pricing === 3) return <Typography><b>Pricing:</b> $$$</Typography>
+  }
+  return (
+    <MaterialTable
+      columns={columns}
+      data={companies}
+      title="Companies"
+      detailPanel={({rowData}) => {
+        return (
+          <Grid style={{width: "100%", padding: 10}} container justifyContent="center" alignItems="center">
+            <Grid item xs={12} md={6}>
+              <Typography ><b>Address:</b> {rowData.address}</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography><b>ZIP:</b> {rowData.zip}</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography ><b>Rating:</b> {rowData.rating}</Typography>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              {calculatePricing(rowData.pricing)}
+            </Grid>
+            <Grid item xs={12}>
+              <Typography><b>Phone:</b> {rowData.phone}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              {renderWebsites(rowData.website)}
+            </Grid>
+          </Grid>
+        );
+      }}
+    />
+  );
 };
 
 export default LookupTable;
