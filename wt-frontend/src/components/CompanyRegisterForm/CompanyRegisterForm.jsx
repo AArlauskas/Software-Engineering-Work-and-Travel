7bc8ffe62240b5f1fb2ab2db1fbba691df1244d9
Aurimas <auruxxas.ar@gmail.com>
2022-01-05 01:38:38 +0000

    frontend form errors and controlled states

--- wt-frontend/src/components/CompanyRegisterForm/CompanyRegisterForm.jsx
+++ wt-frontend/src/components/CompanyRegisterForm/CompanyRegisterForm.jsx
@@ -7,13 +7,78 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
+import { useState } from "react";
 import states from "../../constants/states";
 
-const CompanyRegisterForm = () => {
+const CompanyRegisterForm = ({ onSignUp }) => {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [state, setState] = useState(states[0].name);
+  const [zip, setZip] = useState("");
+  const [location, setLocation] = useState("");
+  const [address, setAddress] = useState("");
+
+  const [showNameError, setShowNameError] = useState(false);
+  const [showMailError, setShowMailError] = useState(false);
+  const [showPhoneError, setShowPhoneError] = useState(false);
+  const [showStateError, setShowStateError] = useState(false);
+  const [showZipError, setShowZipError] = useState(false);
+  const [showLocationError, setShowLocationError] = useState(false);
+  const [showAddressError, setShowAddressError] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    resetErrors();
+
+    if (name.length < 4 || name.length > 30) {
+      setShowNameError(true);
+      return;
+    }
+    if (
+      mail.length < 5 ||
+      !mail.includes("@") ||
+      !mail.includes(".") ||
+      mail.length > 30
+    ) {
+      setShowMailError(true);
+      return;
+    }
+    if (phone.length < 6 || phone.length > 12) {
+      setShowPhoneError(true);
+      return;
+    }
+    if (!states.includes(state)) {
+      setShowStateError(true);
+      return;
+    }
+    if (zip.length < 4 || zip.length > 10) {
+      setShowZipError(true);
+      return;
+    }
+    if (location.length < 3 || location.length > 30) {
+      setShowLocationError(true);
+      return;
+    }
+    if (address.length < 3 || address.length > 30) {
+      setShowAddressError(true);
+      return;
+    }
+  };
+
+  const resetErrors = () => {
+    setShowNameError(false);
+    setShowMailError(false);
+    setShowPhoneError(false);
+    setShowStateError(false);
+    setShowZipError(false);
+    setShowLocationError(false);
+    setShowAddressError(false);
+  };
   return (
     <Paper elevation={4}>
       <CardContent>
-        <form>
+        <form onSubmit={onSubmit}>
           <Grid
             container
             justifyContent="center"
@@ -26,29 +91,87 @@ const CompanyRegisterForm = () => {
               </Typography>
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth placeholder="Companie's name" type="text" />
+              <TextField
+                fullWidth
+                placeholder="Companie's name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                error={showNameError}
+                helperText={showNameError && "Invalid company name"}
+              />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField fullWidth placeholder="email" type="email" />
+              <TextField
+                fullWidth
+                placeholder="email"
+                type="email"
+                value={mail}
+                onChange={(e) => setMail(e.target.value)}
+                error={showMailError}
+                helperText={showMailError && "Invalid email address"}
+              />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField fullWidth placeholder="phone" type="tel" />
+              <TextField
+                fullWidth
+                placeholder="phone"
+                type="tel"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                error={showPhoneError}
+                helperText={showPhoneError && "Invalid phone number"}
+              />
             </Grid>
             <Grid item md={6} xs={12}>
               <Autocomplete
                 options={states}
                 fullWidth
                 getOptionLabel={(s) => s.name}
+                value={state}
+                onChange={(e, newValue) => setState(newValue)}
                 renderInput={(params) => (
-                  <TextField {...params} label="State" />
+                  <TextField
+                    {...params}
+                    label="State"
+                    error={showStateError}
+                    helperText={showStateError && "Invalid state choice"}
+                  />
                 )}
               />
             </Grid>
             <Grid item md={6} xs={12}>
-              <TextField fullWidth placeholder="Postal code" type="text" />
+              <TextField
+                fullWidth
+                placeholder="Postal/ZIP code"
+                type="text"
+                value={zip}
+                onChange={(e) => setZip(e.target.value)}
+                error={showZipError}
+                helperText={showZipError && "Invalid zip code"}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField fullWidth placeholder="Address" type="text" />
+              <TextField
+                fullWidth
+                placeholder="Location/City"
+                type="text"
+                value={location}
+                onChange={(e) => setLocation(e.target.value)}
+                error={showLocationError}
+                helperText={showLocationError && "Invalid location"}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                fullWidth
+                placeholder="Address"
+                type="text"
+                value={address}
+                onChange={(e) => setAddress(e.target.value)}
+                error={showAddressError}
+                helperText={showAddressError && "Invalid address"}
+              />
             </Grid>
             <Grid item xs={12}>
               <Button variant="outlined" fullWidth color="secondary">
