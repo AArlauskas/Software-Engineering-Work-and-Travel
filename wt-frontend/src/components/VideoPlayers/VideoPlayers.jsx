43b7753cc7e054c492bdd735b800d521a7b89991
Ana <ana2000stip@gmail.com>
2022-01-20 00:26:33 +0000

    Video in instructions fixed

--- wt-frontend/src/components/VideoPlayers/VideoPlayers.jsx
+++ wt-frontend/src/components/VideoPlayers/VideoPlayers.jsx
@@ -1,17 +1,19 @@
-import instructions from "../../assets/instructions.wmv";
+import instructions from "../../assets/instructions1.mp4";
 import VideoPlayer from "react-video-js-player"
-
+import posterpic from "../../assets/apppass.jpg";
 
 
 const VideoPlayers = () => {
     const videoSrc=instructions
+    const pic=posterpic
         return (
            <div className="App">
                <h2>Tutorial</h2>
                <VideoPlayer
                src={videoSrc}
-               widht="720"
-               height="420"/>
+               poster={pic}
+               widht="520"
+               height="220"/>
            </div>
         )
 }
