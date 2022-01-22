f380e3ea73a2fb1470a13fa3f15d0c89081daf24
Ana <ana2000stip@gmail.com>
2022-01-22 17:21:56 +0000

    Adding logs for every controller

--- wt-backend/src/main/java/wt/backend/controllers/PaymentsController.java
+++ wt-backend/src/main/java/wt/backend/controllers/PaymentsController.java
@@ -14,8 +14,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import wt.backend.enums.LogType;
 import wt.backend.enums.UserRoles;
 import wt.backend.models.User;
+import wt.backend.services.LogsService;
 import wt.backend.services.UsersService;
 import wt.backend.utils.StripeClient;
 
@@ -33,6 +35,9 @@ public class PaymentsController {
     @Autowired
     private UsersService usersService;
 
+    @Autowired
+    private LogsService logsService;
+
     @GetMapping("checkout")
     @PreAuthorize("hasRole('BASIC')")
     public ResponseEntity<?> checkout(Authentication authentication) {
@@ -42,6 +47,9 @@ public class PaymentsController {
             if(user == null) return ResponseEntity.notFound().build();
             if(!user.getRole().equals(UserRoles.BASIC.toString())) return ResponseEntity.badRequest().body("Only BASIC users can purchase");
             Session session = stripeClient.getPaymentSession(user.getId());
+
+            logsService.log(LogType.PAYMENT_GET, "User with id " + user.getId() + "made a payment");
+
             return ResponseEntity.ok(session.getUrl());
         }
         catch (StripeException e)
@@ -71,6 +79,8 @@ public class PaymentsController {
                 Long userId = Long.parseLong(session.getMetadata().get("userId"));
                 usersService.upgradeUser(userId);
                 System.out.println("Upgraded user with id of " + userId);
+
+                logsService.log(LogType.UPGRADE_PROFILE, "User with id " + userId + "upgraded his profile");
             }
 
             return ResponseEntity.ok().build();
--- wt-backend/src/main/java/wt/backend/models/Log.java
+++ wt-backend/src/main/java/wt/backend/models/Log.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
-@NoArgsConstructor
 @AllArgsConstructor
 public class Log {
     @Id
@@ -23,9 +23,19 @@ public class Log {
     @Setter
     private String message;
 
+    @Getter
+    @Setter
+    private Date timestamp;
+
+    public Log()
+    {
+        timestamp = new Date();
+    }
+
     public Log(String type, String message)
     {
         this.type = type;
         this.message = message;
+        timestamp = new Date();
     }
 }
