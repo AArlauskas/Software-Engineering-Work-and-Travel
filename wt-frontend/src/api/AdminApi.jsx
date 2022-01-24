989572ec704017c405fa573b6eea6e84af84f76f
Aurimas <auruxxas.ar@gmail.com>
2022-01-24 10:34:42 +0000

    Updating new urls for production

--- wt-frontend/src/api/AdminApi.jsx
+++ wt-frontend/src/api/AdminApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://deti-engsoft-09:8080/api",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${window.localStorage.getItem("token")}`,
